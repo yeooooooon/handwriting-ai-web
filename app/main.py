63df431a781c39c396dc67b8bf5f9e12d21103a0
KOSOOJAE <202003305@g.seoil.ac.kr>
2025-07-17 10:42:54 +0900

    Initial commit: FastAPI + static site

---
+++ app/main.py
@@ -0,0 +1,23 @@
+# # main.py
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "AI 손글씨 교정 API입니다"}
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
+
+app = FastAPI()
+
+# 정적 파일(css, js 등) 서빙
+app.mount("/static", StaticFiles(directory="app/css"), name="static")
+
+# index.html 서빙
+@app.get("/")
+def serve_index():
+    return FileResponse(os.path.join("app/public", "index.html"))
