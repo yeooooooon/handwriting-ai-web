0876d40b1e7aa915d5d711ba1291e032ca160398
KOSOOJAE <202003305@g.seoil.ac.kr>
2025-07-20 17:26:56 +0900

    반응형, UI, 폰트

--- app/main.py
+++ app/main.py
@@ -1,36 +1,30 @@
-# # main.py
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# @app.get("/")
-# def read_root():
-#     return {"message": "AI 손글씨 교정 API입니다"}
-
+# main.py
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from io import BytesIO
-from PIL import Image
+from pathlib import Path
 import os
 
 from app.handwriting_feedback_api import router as handwriting_router
 
+BASE_DIR = Path(__file__).resolve().parent  # main.py가 있는 폴더.
+
 app = FastAPI()
 app.include_router(handwriting_router)
 
-# # ✅ 하위 FastAPI 앱 통합 (라우트 등록)
-#app.mount("/evaluate", handwriting_app)
-
-# 정적 파일(css, js 등) 서빙
-app.mount("/static", StaticFiles(directory="app/css"), name="static")
+# ─── 절대경로로 static mount ───────────────────────────
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "css"),
+    name="static"
+)
 
 # index.html 서빙
-@app.get("/")
+@app.get("/", response_class=FileResponse)
 def serve_index():
-    return FileResponse(os.path.join("app/public", "index.html"))
+    return BASE_DIR / "public" / "index.html"
 
 # test.html 서빙
-@app.get("/test")
+@app.get("/test", response_class=FileResponse)
 def serve_test():
-    return FileResponse(os.path.join("app/public", "test.html"))
+    return BASE_DIR / "public" / "test.html"
